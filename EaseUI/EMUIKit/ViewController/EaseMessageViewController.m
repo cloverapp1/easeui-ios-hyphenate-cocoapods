bb66a597a3075e915ac8a1d4fa7a7688dfe979dc
suyy2015 <suyiyao2015@163.com>
2017-08-14 17:59:32 +0800

    修复消息显示bug

--- EaseUI/EMUIKit/ViewController/EaseMessageViewController.m
+++ EaseUI/EMUIKit/ViewController/EaseMessageViewController.m
@@ -2033,6 +2033,7 @@
 #pragma mark - notifycation
 - (void)didBecomeActive
 {
+    self.messageTimeIntervalTag = -1;
     self.dataArray = [[self formatMessages:self.messsagesSource] mutableCopy];
     [self.tableView reloadData];
     
